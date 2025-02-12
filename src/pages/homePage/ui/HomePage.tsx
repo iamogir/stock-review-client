df29ff72aec19caf8e428853de2f75e69ef2131f
iamogir <ia.mogirevsky@gmail.com>
2025-02-13 00:49:13 +0200

    added list for products

--- src/pages/homePage/ui/HomePage.tsx
+++ src/pages/homePage/ui/HomePage.tsx
@@ -1,14 +1,30 @@
 import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {getAllProductsAsyncAction} from "../../../features/products/productAcyncActions.ts";
+import {Product} from "../../../entities/product/model/types.ts";
 
 const HomePage = () => {
 
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    const {products} = useSelector((state) => state.homePage);
+
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        dispatch(getAllProductsAsyncAction)
+    }, [])
 
     return (
         <div>
             <button onClick={() => navigate("/warehouse")}>to warehouse</button>
             <button>add product</button>
             <h3>Status</h3>
+            <br/>
+            {products.map((pr: Product) => <li>{pr.name}</li>)}
         </div>
     );
 };
