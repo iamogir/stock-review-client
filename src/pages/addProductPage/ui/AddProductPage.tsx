2a79f58e227ada36ef1b1b381f6ee3a94eef1442
iamogir <ia.mogirevsky@gmail.com>
2025-02-19 23:39:01 +0200

    form fields

--- src/pages/addProductPage/ui/AddProductPage.tsx
+++ src/pages/addProductPage/ui/AddProductPage.tsx
@@ -4,8 +4,26 @@ const AddProductPage = () => {
     return (
         <div>
             <form>
-                <label>smth</label>
+                <label>Name</label>
                 <input name={'name'} />
+                <label>Category</label>
+                <input name={'category'} />
+                <label>Weight</label>
+                <input name={'weight'} />
+                <label>Unit</label>
+                <input name={'unit'} />
+                <label>Exp date</label>
+                <input name={'expDate'} />
+                <label>Barcode</label>
+                <input name={'barcode'} />
+                <label>Supplier</label>
+                <input name={'supplier'} />
+                <label>Price</label>
+                <input name={'price'} />
+                <label>Location</label>
+                <input name={'location'} />
+                <label>Status</label>
+                <input name={'status'} />
             </form>
         </div>
     );
