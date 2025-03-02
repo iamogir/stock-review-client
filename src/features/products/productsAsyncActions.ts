8b5d414e4740ba1a1ad9a7101b7d96f6fc725fcf
iamogir <ia.mogirevsky@gmail.com>
2025-03-02 22:05:55 +0200

    get expiring soon async action

--- src/features/products/productsAsyncActions.ts
+++ src/features/products/productsAsyncActions.ts
@@ -93,3 +93,32 @@ export const getExpiredProductsAsyncAction = createAsyncThunk<ProductsResponse>(
         }
     }
 )
+
+export const getExpiringSoonProductsAsyncAction = createAsyncThunk<ProductsResponse, number, { rejectValue: string }> (
+    'products/get_expired_soon',
+    async(countDays: number, thunkAPI ): Promise<ProductsResponse | ReturnType<typeof thunkAPI.rejectWithValue>> => {
+        try {
+            const products: Product[] = [];
+            const response = await fetch(API + 'products/get_expired_soon/' + countDays);
+            if (response.status === 200 || response.status === 304) {
+                const json = await response.json();
+
+                json.map((pr: ProductDto) => products.push(fromServerObject(pr)));
+
+                if (products.length === 0) {
+                    throw new Error('No expired products for next ' + countDays + ' days. Great job!')
+                } else {
+                    return { products };
+                }
+
+            } else {
+                throw new Error(response.statusText);
+            }
+        } catch (error) {
+            console.log('get_expired_soon', error);
+            return thunkAPI.rejectWithValue(
+                error instanceof Error ? error.message : 'Something went wrong'
+            );
+        }
+    }
+)
