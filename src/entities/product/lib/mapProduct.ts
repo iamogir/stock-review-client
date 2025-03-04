1f7744381bd424b66ef98ceba719cd3a7a85a07c
iamogir <ia.mogirevsky@gmail.com>
2025-03-04 22:05:20 +0200

    changed mapping

--- src/entities/product/lib/mapProduct.ts
+++ src/entities/product/lib/mapProduct.ts
@@ -1,21 +1,32 @@
-import {Product, ProductDto} from "../model/types.ts";
+import {Product, ProductDto, StockEntry, StockEntryDto} from "../model/types.ts";
 
-export function fromJSON(json: string): Product {
-    const obj = JSON.parse(json);
-    return fromServerObject(obj);
-}
+// export function fromJSON(json: string): StockEntryDto | ProductDto {
+//     const obj = JSON.parse(json);
+//     return fromServerStockEntryObject(obj);
+// }
 
-export function fromServerObject(obj: ProductDto): Product {
+export function fromServerStockEntryObject(obj: StockEntryDto): StockEntry {
+
+    const product = fromServerProductObject(obj.productId);
+
     return {
-        id: obj._id,
-        name: obj.name,
-        category: obj.category,
+        ...product,
+        entryId: obj._id,
         weight: obj.weight,
         unitWeight: obj.unitWeight,
         quantityUnits: obj.quantityUnits,
         expirationDate: obj.expirationDate,
         supplier: obj.supplier,
         storageLocation: obj.storageLocation,
+    }
+}
+
+export function fromServerProductObject(obj: ProductDto): Product {
+    return {
+        productId: obj._id,
+        name: obj.name,
+        category: obj.category,
+        brand: obj.brand,
         status: obj.status,
     }
 }
