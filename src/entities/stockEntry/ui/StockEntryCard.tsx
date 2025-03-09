52d287f5c59526d92cf1bf96467bd6cc0c60060b
iamogir <ia.mogirevsky@gmail.com>
2025-03-09 15:44:31 +0200

    entry knows it parent

--- src/entities/stockEntry/ui/StockEntryCard.tsx
+++ src/entities/stockEntry/ui/StockEntryCard.tsx
@@ -1,7 +1,5 @@
 import style from './stockEntryCard.module.css'
-import {Product, StockEntry} from "../../product/model/types.ts";
-import {useSelector} from "react-redux";
-import {RootState} from "../../../app/redux/store.ts";
+import {StockEntry} from "../../product/model/types.ts";
 import DeleteProductButton from "../../../features/products/deleteProductButton/ui/DeleteProductButton.tsx";
 
 interface Props {
@@ -11,22 +9,19 @@ interface Props {
 const StockEntryCard = ({stockEntry}: Props) => {
 
     const dateExpiration = new Date(stockEntry.expirationDate);
-    const {products, loading} = useSelector((state: RootState) => state.products);
 
-    const product: Product | undefined = products?.find(p => p.id === stockEntry.id);
-
-    return ( loading ? <div>Loading...</div> :
+    return (
             <div>
                 <br/>
                 <hr/>
-                <h2 className={style.temp2}>{product?.name}</h2>
+                <h2 className={style.temp2}>{stockEntry.productInfo?.name}</h2>
                 <h3>Supplier: {stockEntry.supplier}</h3>
-                <h4>Category: {product?.category}</h4>
+                <h4>Category: {stockEntry.productInfo?.category}</h4>
                 <p>In stock: {stockEntry.quantityUnits} units</p>
-                <p>General weight: {stockEntry.weight} {product?.unitWeight}</p>
+                <p>General weight: {stockEntry.weight} {stockEntry.productInfo?.unitWeight}</p>
                 <p className={style.temp}>Sell by: {dateExpiration.getDate()}/{dateExpiration.getUTCMonth() + 1}/{dateExpiration.getFullYear()}</p>
                 <p>Location: {stockEntry.storageLocation}</p>
-                <h3>Status: {product?.status ? 'in stock' : 'out of stock'}</h3>
+                <h3>Status: {stockEntry.productInfo?.status ? 'in stock' : 'out of stock'}</h3>
                 <DeleteProductButton id={stockEntry.id ?? ''} />
                 <br/>
             </div>
