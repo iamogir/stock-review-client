7b2de2cba6137c380e38f89bef11fc895fd13c03
iamogir <ia.mogirevsky@gmail.com>
2025-03-10 12:11:34 +0200

    second right checking

--- src/features/products/actions/filteredStockEntriesAsyncActions.ts
+++ src/features/products/actions/filteredStockEntriesAsyncActions.ts
@@ -8,11 +8,13 @@ const API = import.meta.env.VITE_API_URL;
 export const getExpiredProductsAsyncAction = createAsyncThunk<StockEntryResponse, { rejectValue: string }>(
     'stock_entry/get_expired_products',
     async(_, thunkAPI): Promise<StockEntryResponse | ReturnType<typeof thunkAPI.rejectWithValue>> => {
-        try{
-            console.log('stock_entry/get_expired_products')
+        try {
             const { products } = thunkAPI.getState() as RootState;
             const productsArr = products.products;
             const stockEntriesArr = products.stockEntries;
+            if (!productsArr) {
+                return thunkAPI.rejectWithValue('No products are found');
+            }
             if (stockEntriesArr && stockEntriesArr.length > 0 || productsArr && productsArr.length > 0) { //TODO: this right!!!
                 console.log('Entries are already loaded')
                 return thunkAPI.rejectWithValue('Entries are already loaded');
@@ -47,6 +49,9 @@ export const getExpiringSoonProductsAsyncAction = createAsyncThunk<StockEntryRes
             const { products } = thunkAPI.getState() as RootState;
             const productsArr = products.products;
             const stockEntriesArr = products.stockEntries;
+            if (!productsArr) {
+                return thunkAPI.rejectWithValue('No products are found');
+            }
             if (!stockEntriesArr || !productsArr || stockEntriesArr.length === 0 || productsArr.length === 0) {
                 return thunkAPI.rejectWithValue('Entries are already loaded');
             }
