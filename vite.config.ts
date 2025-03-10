832d7c4b836399415b96978aaa1ec10718fd621e
iamogir <ia.mogirevsky@gmail.com>
2025-03-10 14:09:32 +0200

    new import settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: "/src/",
+      app: '/src/app/',
+      pages: '/src/pages/',
+      widgets: '/src/widgets/',
+      features: '/src/features/',
+      shared: '/src/shared/',
+    },
+  }
+
 })
