fea06e8a811a3eb17af253bd8a6f8b65520023bb
iamogir <ia.mogirevsky@gmail.com>
2025-03-25 21:44:31 +0200

    some changes

--- src/features/products/deleteTempProductButton/ui/DeleteTempProductButton.tsx
+++ src/features/products/deleteTempProductButton/ui/DeleteTempProductButton.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {removeOneEntry} from "features/products";
+import style from './deleteTempProductButton.css'
 
-export const DeleteTempProductButton = () => {
+interface Props {
+    index: number
+}
+
+export const DeleteTempProductButton = ({ number }: Props) => {
     return (
         <div>
-
+            <button onClick={() => dispatch(removeOneEntry(number))}>Delete</button>
         </div>
     );
 };
