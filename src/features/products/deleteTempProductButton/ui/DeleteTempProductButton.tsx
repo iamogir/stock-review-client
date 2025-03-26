db48d67ab47b76b1a99f3097136978ccc63bd5db
iamogir <ia.mogirevsky@gmail.com>
2025-03-27 00:01:23 +0200

    fixed removing

--- src/features/products/deleteTempProductButton/ui/DeleteTempProductButton.tsx
+++ src/features/products/deleteTempProductButton/ui/DeleteTempProductButton.tsx
@@ -3,20 +3,17 @@ import {AppDispatch} from "app/redux";
 import {useDispatch} from "react-redux";
 import {ActionCreatorWithPayload} from "@reduxjs/toolkit";
 
-interface Props<T extends string | number> {
-    index: T,
-    deleteFunc: ActionCreatorWithPayload<T>;
+interface Props {
+    index: number,
+    deleteFunc: ActionCreatorWithPayload<number>;
 }
 
-export const DeleteTempProductButton = <T extends string | number>({ index, deleteFunc }: Props<T>) => {
+export const DeleteTempProductButton = ({ index, deleteFunc }: Props) => {
 
     const dispatch = useDispatch<AppDispatch>();
 
-    const temp = () => {
-        dispatch(deleteFunc(index));
-    }
-
     return (
-        <button className={style.color} onClick={temp}>Delete</button>
+        <button className={style.color} onClick={() => dispatch(deleteFunc(index))}>Delete</button>
+
     );
 };
